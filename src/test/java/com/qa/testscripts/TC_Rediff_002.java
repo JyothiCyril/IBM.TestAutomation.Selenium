04b326d2e0fb2511c63942600ddc8faee29d563d
TL - JyothiCyril <49202864+JyothiCyril@users.noreply.github.com>
2022-03-24 15:45:34 +0530

    Added to Branch XYZ

---
+++ src/test/java/com/qa/testscripts/TC_Rediff_002.java
@@ -0,0 +1,12 @@
+package com.qa.testscripts;
+
+import org.testng.annotations.Test;
+
+public class TC_Rediff_002 extends TestBase{
+	
+	@Test
+	public void Login() {
+		// Loc
+	}
+
+}
---
+++ src/test/java/com/qa/testscripts/TC_Rediff_003.java
@@ -0,0 +1,12 @@
+package com.qa.testscripts;
+
+import org.testng.annotations.Test;
+
+public class TC_Rediff_003 extends TestBase{
+
+	@Test
+	public void userReg() {
+		
+	}
+	
+}
